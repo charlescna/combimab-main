70ef71e0e4b642e28710029fd8534d168bd4a9ea
charlescna <122284631+charlescna@users.noreply.github.com>
2023-11-20 19:37:12 -0600

    updating css and homepage in new frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import HomePage from "./Pages/HomePage";
+import HcpRegistration from "./Pages/HcpRegistration";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <BrowserRouter>
+        <div className="App">
+ 
+          <div id= "page-body" >
+              <Routes>
+                <Route path ="/" element={<HomePage/>} />
+              </Routes>
+          </div>
+ 
+          <div id="Calculation">
+            <Routes>
+              <Route path='/hcp-support' element={<HcpRegistration/>}/>
+            </Routes>
+          </div>
+        </div>
+      </BrowserRouter>
+ 
   );
 }
-
 export default App;
