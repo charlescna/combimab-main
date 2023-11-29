0f493c6d35b6c09ff66874f8b6dc699afc9dab25
MohaimenALA <122465051+MohaimenALA@users.noreply.github.com>
2023-11-28 22:27:17 -0330

    Moved fills with steps prep page

--- combimab-frontend/src/pages/HomePage.js
+++ combimab-frontend/src/pages/HomePage.js
@@ -1,27 +1,15 @@
 
-
-
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import '../App.css';
-import { GoogleLoginButton } from "react-social-login-buttons";
-import { LoginSocialGoogle } from "reactjs-social-login";
 
 function HomePage() {
   const [patientWeight, setPatientWeight] = useState('');
   const [generatedDose, setGeneratedDose] = useState('');
+
   const handleCalculate = () => {
-      const dose = '**Numb***';
+    const dose = '**Numb***';
     setGeneratedDose(dose);
-  };
-
-  const redirectToGoogleAuthentication = () => {
-    const googleAuthenticationURL = 'https://accounts.google.com'; 
-    window.location.href = googleAuthenticationURL;
-  };
-
-  const showGoogleAuthComponent = () => {
-    setShowGoogleAuth(true);
   };
 
   return (
@@ -30,19 +18,19 @@ function HomePage() {
       <div className="box">
         <img src="HomePagePhoto.png" alt="Bio" />
 
-            <div className="box-content">
-              <h2>COMBIMAB IS</h2>
-              <p>Weight-based Nmab® 300 mg/3mL dosing calculator for patients with PN</p>
-              <div className="calculator-bar">
-                <label>Patient weight:</label>
-                <input
-                  type="text"
-                  value={patientWeight}
-                  onChange={(e) => setPatientWeight(e.target.value)}
-                  placeholder="patient weight in Kg"
-                />
-                <button onClick={handleCalculate}>CALCULATE</button>
-              </div>
+        <div className="box-content">
+          <h2>COMBIMAB IS</h2>
+          <p>Weight-based Nmab® 300 mg/3mL dosing calculator for patients with PN</p>
+          <div className="calculator-bar">
+            <label>Patient weight:</label>
+            <input
+              type="text"
+              value={patientWeight}
+              onChange={(e) => setPatientWeight(e.target.value)}
+              placeholder="patient weight in Kg"
+            />
+            <button onClick={handleCalculate}>CALCULATE</button>
+          </div>
           <table className="dose-table">
             <thead>
               <tr>
@@ -60,30 +48,7 @@ function HomePage() {
           <p>For Health Care professionals support to have infusion specifications</p>
           <p>Steps for preparation, Calendar generation please click HCP Support </p>
 
-          
-          <button className="hcp-support-button" onClick={showGoogleAuthComponent}>
-            HCP Support
-          </button>
-
-          
-          {showGoogleAuth && (
-            <div>
-              <LoginSocialGoogle
-                client_id={"102495660704-402m26dsokb0rk6ljbslj0t59boi4dfu.apps.googleusercontent.com"}
-                scope="openid profile email"
-                discoveryDocs="claims_supported"
-                access_type="offline"
-                onResolve={({ provider, data }) => {
-                  console.log(provider, data);
-                }}
-                onReject={(err) => {
-                  console.log(err);
-                }}
-              >
-                <GoogleLoginButton />
-              </LoginSocialGoogle>
-            </div>
-          )}
+          <Link className="hcp-support-button" to="/hcp-support-button">HCP Support</Link>
 
           <p className="disclaimer">This web App is not for medical purpose. All information here is dummy data and is done as a project for students in CAN for CP3540.</p>
         </div>
--- combimab-frontend/src/pages/PreparationGuidance.js
+++
@@ -1,6 +0,0 @@
-const PreparationGuidance = ()=> {
-    return (<h1> Step by step guidance for preparation</h1>
-
-    );
-}
-export default PreparationGuidance;
